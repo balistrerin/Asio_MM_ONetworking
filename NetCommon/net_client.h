1f18bba45f20fc5b68c2ba26a1d69729734481cc
Nick Balistreri <balistrerin@gmail.com>
2025-01-27 15:33:46 -0800

    updates

--- NetCommon/net_client.h
+++ NetCommon/net_client.h
@@ -10,53 +10,65 @@ namespace olc
 		template <typename T>
 		class client_interface
 		{
-			client_interface() : m_socket(m_context)
+		public:
+			client_interface()
 			{
-
 			}
 
 			virtual ~client_interface()
 			{
+				// If the client is destroyed, always try and disconnect from server
 				Disconnect();
 			}
 
 		public:
+			// Connect to server with hostname/ip-address and port
 			bool Connect(const std::string& host, const uint16_t port)
 			{
-				try {
-
-					m_connection = std::make_unique<connection<T>>();
-
+				try
+				{
+					// Resolve hostname/ip-address into tangiable physical address
 					asio::ip::tcp::resolver resolver(m_context);
-					m_endpoints = resolver.resolve(host, std::to_string(port));
+					asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(host, std::to_string(port));
 
-					m_connection->ConnectToServer(m_endpoints);
+					// Create connection
+					m_connection = std::make_unique<connection<T>>(connection<T>::owner::client, m_context, asio::ip::tcp::socket(m_context), m_qMessagesIn);
 
+					// Tell the connection object to connect to server
+					m_connection->ConnectToServer(endpoints);
+
+					// Start Context Thread
+					thrContext = std::thread([this]() { m_context.run(); });
 				}
-				catch(std::exception& e){
-					std::cerr << "Client Exception " << e.what() << "\n";
+				catch (std::exception& e)
+				{
+					std::cerr << "Client Exception: " << e.what() << "\n";
 					return false;
 				}
-
-
 				return true;
 			}
 
+			// Disconnect from server
 			void Disconnect()
 			{
+				// If connection exists, and it's connected then...
 				if (IsConnected())
 				{
+					// ...disconnect from server gracefully
 					m_connection->Disconnect();
 				}
 
+				// Either way, we're also done with the asio context...				
 				m_context.stop();
-
+				// ...and its thread
 				if (thrContext.joinable())
 					thrContext.join();
 
+				// Destroy the connection object
 				m_connection.release();
 			}
 
+			// Check if client is actually connected to a server
 			bool IsConnected()
 			{
 				if (m_connection)
@@ -65,24 +77,31 @@ namespace olc
 					return false;
 			}
 
-			tsqueue<owned_message<T>>& Incoming()
+		public:
+			// Send message to server
+			void Send(const message<T>& msg)
 			{
-				return m_qMessageIn;
+				if (IsConnected())
+					m_connection->Send(msg);
 			}
 
+			// Retrieve queue of messages from server
+			tsqueue<owned_message<T>>& Incoming()
+			{
+				return m_qMessagesIn;
+			}
 
 		protected:
+			// asio context handles the data transfer...
 			asio::io_context m_context;
+			// ...but needs a thread of its own to execute its work commands
 			std::thread thrContext;
-			asio::ip::tcp::socket m_socket;
+			// The client has a single instance of a "connection" object, which handles data transfer
 			std::unique_ptr<connection<T>> m_connection;
 
-
 		private:
-			tsqueue<owned_message<T>> m_qMessageIn;
-
+			// This is the thread safe queue of incoming messages from server
+			tsqueue<owned_message<T>> m_qMessagesIn;
 		};
-
 	}
-
 }
--- NetCommon/net_tsqueue.h
+++ NetCommon/net_tsqueue.h
@@ -6,7 +6,7 @@ namespace olc
 {
 	namespace net
 	{
-		template <typename T>
+		template<typename T>
 		class tsqueue
 		{
 		public:
@@ -15,48 +15,21 @@ namespace olc
 			virtual ~tsqueue() { clear(); }
 
 		public:
+			// Returns and maintains item at front of Queue
 			const T& front()
 			{
 				std::scoped_lock lock(muxQueue);
 				return deqQueue.front();
 			}
 
+			// Returns and maintains item at back of Queue
 			const T& back()
 			{
 				std::scoped_lock lock(muxQueue);
 				return deqQueue.back();
 			}
 
-			void push_back(const T& item)
-			{
-				std::scoped_lock lock(muxQueue);
-				deqQueue.emplace_back(std::move(item));
-			}
-
-			void push_front(const T& item)
-			{
-				std::scoped_lock lock(muxQueue);
-				deqQueue.emplace_front(std::move(item));
-			}
-
-			bool empty()
-			{
-				std::scoped_lock lock(muxQueue);
-				return deqQueue.empty();
-			}
-
-			size_t count()
-			{
-				std::scoped_lock lock(muxQueue);
-				return deqQueue.size();
-			}
-
-			void clear()
-			{
-				std::scoped_lock lock(muxQueue);
-				deqQueue.clear();
-			}
-
+			// Removes and returns item from front of Queue
 			T pop_front()
 			{
 				std::scoped_lock lock(muxQueue);
@@ -65,6 +38,7 @@ namespace olc
 				return t;
 			}
 
+			// Removes and returns item from back of Queue
 			T pop_back()
 			{
 				std::scoped_lock lock(muxQueue);
@@ -73,12 +47,61 @@ namespace olc
 				return t;
 			}
 
+			// Adds an item to back of Queue
+			void push_back(const T& item)
+			{
+				std::scoped_lock lock(muxQueue);
+				deqQueue.emplace_back(std::move(item));
+
+				std::unique_lock<std::mutex> ul(muxBlocking);
+				cvBlocking.notify_one();
+			}
+
+			// Adds an item to front of Queue
+			void push_front(const T& item)
+			{
+				std::scoped_lock lock(muxQueue);
+				deqQueue.emplace_front(std::move(item));
+
+				std::unique_lock<std::mutex> ul(muxBlocking);
+				cvBlocking.notify_one();
+			}
+
+			// Returns true if Queue has no items
+			bool empty()
+			{
+				std::scoped_lock lock(muxQueue);
+				return deqQueue.empty();
+			}
+
+			// Returns number of items in Queue
+			size_t count()
+			{
+				std::scoped_lock lock(muxQueue);
+				return deqQueue.size();
+			}
+
+			// Clears Queue
+			void clear()
+			{
+				std::scoped_lock lock(muxQueue);
+				deqQueue.clear();
+			}
+
+			void wait()
+			{
+				while (empty())
+				{
+					std::unique_lock<std::mutex> ul(muxBlocking);
+					cvBlocking.wait(ul);
+				}
+			}
 
 		protected:
 			std::mutex muxQueue;
 			std::deque<T> deqQueue;
+			std::condition_variable cvBlocking;
+			std::mutex muxBlocking;
 		};
-
 	}
-
 }
